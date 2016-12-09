7864edb7bb42adbf0861d78dfbad484729f0143f
Michal Hlaváček <HlavacekMichal1991@gmail.com>
2016-12-10 00:20:29 +0100

    Update solvemedia.rb

--- lib/solvemedia.rb
+++ lib/solvemedia.rb
@@ -14,9 +14,6 @@ module SolveMedia
   end
   
   def self.check_for_keys!
-    if !File.exist?(CONFIG_FILE) || CONFIG.nil? || CONFIG['C_KEY'].nil? || CONFIG['V_KEY'].nil? || CONFIG['H_KEY'].nil?
-      raise AdCopyError, "Solve Media API keys not found. Keys can be obtained at #{SIGNUP_URL}"
-    end
   end
 end
 
